cec528ce709436cd077fedcccc7d27756f7ce7a5
nicrusso7 <dott.nicolarusso@gmail.com>
2021-06-16 00:38:55 +0200

    mpc new offsets

--- robot_gym/model/robots/ghost/ctrl_constants.py
+++ robot_gym/model/robots/ghost/ctrl_constants.py
@@ -7,7 +7,7 @@ from mpc_controller import gait_generator as gait_generator_lib
 # -------------------------------------------------
 MPC_BODY_MASS = 190 / 9.8
 MPC_BODY_INERTIA = (0.07335, 0, 0, 0, 0.25068, 0, 0, 0, 0.25447)
-MPC_BODY_HEIGHT = 0.42
+MPC_BODY_HEIGHT = 0.44
 MPC_VELOCITY_MULTIPLIER = 1.0
 
 STANCE_DURATION_SECONDS = [0.3] * 4  # For faster trotting (v > 1.5 ms reduce this to 0.13s).
@@ -36,9 +36,9 @@ INIT_LEG_STATE = (
     gait_generator_lib.LegState.SWING,
 )
 
-VX_OFFSET = 0.
-VY_OFFSET = 0.08
-WZ_OFFSET = -0.025
+VX_OFFSET = 0.025
+VY_OFFSET = 0.045
+WZ_OFFSET = -0.04
 
 # -------------------------------------------------
 # Pose Controller
